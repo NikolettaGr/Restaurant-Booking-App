cf24ab82c83084c17a73ef42ee0d15f37be7de0d
NikolettaGr <grishaberN@outlook.com>
2023-10-11 10:23:27 +0200

    Register model

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    
+    list_display = ('user', 'full_name',
+                    'date', 'time',
+                    'table', 'phone_number')
+    search_fields = ('full_name',
+                     'date', 'time',
+                     'phone_number')
+    list_filter = ('full_name',
+                   'date', 'time', 'table',
+                   'phone_number')
