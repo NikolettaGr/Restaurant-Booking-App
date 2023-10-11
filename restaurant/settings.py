c51c2fe9f3ef148cfab4b830a7aba1f4eef8953e
NikolettaGr <grishaberN@outlook.com>
2023-10-11 12:43:30 +0200

    Add allauth.socialaccount to installed apps and templates directory to TEMPLATES

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
     'booking',
 ]
 
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'restaurant.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
