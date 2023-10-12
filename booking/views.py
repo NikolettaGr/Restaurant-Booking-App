0c0a7673160762440574f1c9f7fb83d24f1e9e6a
NikolettaGr <grishaberN@outlook.com>
2023-10-12 17:29:15 +0200

    Implemented the 'add_booking' view for allowing authenticated users to create and store bookings, ensuring booking date validation and providing feedback messages

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+
+def login(request):
+    return render(request, 'login.html')
+
+@login_required
+def add_booking(request):
+    """
+    Function enables user to make a booking
+    and add it to the database.
+    """
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save()
+            booking.user = request.user
+            booking.save()
+            messages.success(request, 'Booking successful.')
+            return redirect('my_bookings')
+        else:
+            messages.error(request, 'Booking date must be in the future.')
+    form = BookingForm()
+    context = {
+        'form': form
+        }
+    return render(request, 'restaurant/booking.html', context)
+
+
+
