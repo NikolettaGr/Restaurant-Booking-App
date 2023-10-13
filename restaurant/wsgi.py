44b38e6e9951e2aeca0828500e557c652847c91e
NikolettaGr <grishaberN@outlook.com>
2023-10-13 16:09:59 +0200

    Fix restaurnt project structure

--- restaurant/wsgi.py
+++ restaurant/wsgi.py
@@ -10,6 +10,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+# from whitenoise import WhiteNoise
+# from my_project import MyWSGIApp
+
+# application = MyWSGIApp()
+# application = WhiteNoise(application, root="/path/to/static/files")
+# application.add_files("/path/to/more/static/files", prefix="more-files/")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant.settings')
 
