bbe885af4eab9b43414a35ccdac4264857d47eba
NikolettaGr <grishaberN@outlook.com>
2023-10-14 16:52:33 +0200

    Fix secret key

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -24,7 +24,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8-_s@e0g#f4p0$6a_fthdopa6qa#ui16tuo*q#w#yxm83_iu@x'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
