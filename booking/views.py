bd9147f5b64ff9a5f544f73803d2ee7dab768c82
NikolettaGr <grishaberN@outlook.com>
2023-10-14 19:03:20 +0200

    Add view_booking function

--- booking/views.py
+++ booking/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import BookingForm
 
 
 
 
-def index(request):
-     """
+def home(request):
+    """
     Function enables user to view the home page.
     """
     return render(request, 'home.html')
@@ -27,14 +28,27 @@ def add_booking(request):
             booking.user = request.user
             booking.save()
             messages.success(request, 'Booking successful.')
-            return redirect('my_bookings')
+            return redirect('view_booking')
         else:
             messages.error(request, 'Booking date must be in the future.')
     form = BookingForm()
     context = {
         'form': form
         }
-    return render(request, 'booking.html', context)
+    return render(request, 'booking/booking.html', context)
+
+
+@login_required
+def view_booking(request):
+    """
+    Function enables user to view a booking after
+    it has been made and added to the database.
+    """
+    bookings = Booking.objects.filter(user__in=[request.user])
+    context = {
+        'bookings': bookings
+    }
+    return render(request, 'booking/view_booking.html', context)
 
 
 
