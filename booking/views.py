81f0b43bcd3dd14b501e6fe2f7a23e02e34439b5
NikolettaGr <grishaberN@outlook.com>
2023-10-14 22:54:00 +0200

    Implement booking editing feature and defined get_object_or_404

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -50,6 +50,28 @@ def view_booking(request):
         'bookings': bookings
     }
     return render(request, 'booking/view_booking.html', context)
+
+
+@login_required
+def edit_booking(request, booking_id):
+    """
+    Function enables user to edit a booking after
+    it has been made and added to the database.
+    """
+    book = get_object_or_404(Booking, id=booking_id)
+    if request.method == "POST":
+        form = BookingForm(request.POST, instance=book)
+        if form.is_valid():
+            booking = form.save()
+            booking.user = request.user
+            booking.save()
+            messages.success(request, 'Your booking has been updated.')
+        return redirect('view_booking')
+    form = BookingForm(instance=book)
+    context = {
+        'form': form
+    }
+    return render(request, 'booking/edit_booking.html', context)
 
 
 
