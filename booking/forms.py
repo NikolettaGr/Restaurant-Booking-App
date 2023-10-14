58ee275da5ebb7d07a07b5f9918b95d7cabcf9f4
NikolettaGr <grishaberN@outlook.com>
2023-10-14 21:17:24 +0200

    Add date widget

--- booking/forms.py
+++ booking/forms.py
@@ -4,4 +4,9 @@ from .models import Booking
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['full_name', 'date', 'time', 'table', 'questions']
+        fields = ['full_name', 'phone_number', 'date', 'time', 'table', 'questions']
+
+    date = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date'}),
+        help_text='Select a date',
+    )
