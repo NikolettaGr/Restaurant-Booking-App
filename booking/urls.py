fadcafb4fb041f5b47ceba70f7a0f7c35a2a8c77
NikolettaGr <grishaberN@outlook.com>
2023-10-15 21:00:56 +0200

    Implement delete_booking url path

--- booking/urls.py
+++ booking/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home, add_booking, view_booking, edit_booking
+from .views import home, add_booking, view_booking, edit_booking, delete_booking
 
 urlpatterns = [
     path('', home, name='home'),
     path('booking/', add_booking, name='booking'),
     path('view_booking/', view_booking, name='view_booking'),
     path('edit_booking/<int:booking_id>/', edit_booking, name='edit_booking'),
+    path('delete_booking/<int:booking_id>/', delete_booking, name='delete_booking'),
 ]
 
