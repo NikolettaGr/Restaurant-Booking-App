ad49152cf902d41be74c2f5765cbbf5dcfbb2fd8
NikolettaGr <grishaberN@outlook.com>
2023-10-18 03:02:20 +0200

    Implement url path for contact.html

--- booking/urls.py
+++ booking/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, add_booking, view_booking, edit_booking, delete_booking, menu
+from .views import home, add_booking, view_booking, edit_booking, delete_booking, menu, contact
 
 urlpatterns = [
     path('', home, name='home'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('edit_booking/<int:booking_id>/', edit_booking, name='edit_booking'),
     path('delete_booking/<int:booking_id>/', delete_booking, name='delete_booking'),
     path('menu/', menu, name='menu'),
+    path('contact/', contact, name='contact'),
 ]
 
