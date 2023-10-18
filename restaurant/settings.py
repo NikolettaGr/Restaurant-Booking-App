6088446007e0046b7ed80810b8c5a662681c58a5
NikolettaGr <grishaberN@outlook.com>
2023-10-18 04:50:34 +0200

    Uncomment django contrib sites

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.sites',
+    'django.contrib.sites',
     'django.contrib.staticfiles',
     'allauth',
     'allauth.account',
